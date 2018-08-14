e83a3d92b622af29c43a0f6fb6e6d9415243aa9c
Benjamin Horsleben <benjamin@fizkerinc.dk>
2018-08-14 21:52:11 +0200

    Updated command handling to support presets

--- index.js
+++ index.js
@@ -11,36 +11,77 @@ const args = yargs
 			return JSON.parse(fs.readFileSync(configPath, 'utf-8'))
 		},
 	})
-	.option('set', {
-		description: 'The options to change. Can be supplied as either space separated values or given multiple times',
-		type: 'array',
-		coerce: (fromCLI) => {
-			if(!Array.isArray(fromCLI)) {
-				return fromCLI
-			}
-
-			const values = {}
-			for(const val of fromCLI) {
-				if(typeof val !== 'string') {
-					Object.assign(values, val)
-					continue
+	.command(
+		'set <values..>',
+		'Update individual values',
+		yargs => yargs.positional('values', {
+			description: 'One or more values in the format key=value.',
+			type: 'array',
+			coerce: (fromCLI) => {
+				if(!Array.isArray(fromCLI)) {
+					return fromCLI
 				}
-				const [ key, ...rest ] = val.split('=')
-				values[key] = rest.join('=')
-			}
-			return values
-		},
-	})
+
+				const values = {}
+				for(const val of fromCLI) {
+					if(typeof val !== 'string') {
+						Object.assign(values, val)
+						continue
+					}
+					const [ key, ...rest ] = val.split('=')
+					values[key] = rest.join('=')
+				}
+				return values
+			},
+		}),
+	)
+	.command(
+		'preset <preset>',
+		'A preset that can be stored in the config file',
+	)
 	.option('files', {
 		description: 'The files to update. This should be given as part of the config. See config.example.json for examples.',
+		group: 'Config file',
 	})
-	.demandOption(['files', 'config', 'set'])
+	.option('presets', {
+		description: 'The list of presets.',
+		group: 'Config file',
+		default: {},
+	})
+	.demandOption(['files', 'config'])
+	.demandCommand(1, 'You need to set a command')
+	.strict()
 	.argv
 
+let set
+switch(args._[0]) {
+case 'set': {
+	set = args.values
+	break
+}
+case 'preset': {
+	set = args.presets[args.preset]
+	if(!set) {
+		console.log('Unknown preset: ' + args.preset)
+		console.log('Available presets:')
+		console.log(Object.keys(args.presets).join(' '))
+		process.exit(1)
+	}
+	break
+}
+default:
+	console.log('Unknown command: ' + args._[0])
+	process.exit(1)
+}
+
 for(const { file, config } of args.files) {
+	updateFile(file, config, set)
+}
+
+function updateFile(file, config, set) {
 	const filename = file
 	const originalContent = fs.readFileSync(filename, 'utf-8')
-	const newContent = Object.entries(args.set)
+	const newContent = Object.entries(set)
 		.reduce((content, [ configKey, newValue ]) => {
 			const configuration = config[configKey]
 			if(!configuration) {
